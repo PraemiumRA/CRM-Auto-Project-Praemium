c40b623bee50230ce3346a9c7bd67a75b3c80aec
Edgar Tadevosyan <edgartadevosyan25@gmail.com>
2016-11-23 20:18:37 +0400

    was added new functionality about review directory when program start.

--- BLL/FolderWatcher.cs
+++ BLL/FolderWatcher.cs
@@ -9,12 +9,12 @@ namespace BLL
     /// <summary>
     /// Monitoring selected directory
     /// </summary>
-    public class FolderWatcher 
+    public class FolderWatcher
     {
         DirectoryInfo directory;
         DirectoryInfo jsonDirectory;
         FileSystemWatcher watcher;
-        StoreData   storeData;
+        StoreData storeData;
         AppConfiguration appConfiguration = AppConfiguration.GetInstance;
 
         public DirectoryInfo Directory
@@ -27,7 +27,7 @@ namespace BLL
             get { return this.jsonDirectory; }
             set { this.jsonDirectory = value; }
         }
-          
+
         public FolderWatcher()
         {
             this.Directory = new DirectoryInfo(appConfiguration.GetToMnitorDirectory);
@@ -45,28 +45,44 @@ namespace BLL
                 Path = this.Directory.FullName,
                 IncludeSubdirectories = true
             };
-            
+
             watcher.Created += Watcher_Created;
             watcher.Deleted += Watcher_Deleted;
 
             watcher.EnableRaisingEvents = true;
+            ReViewingDirectory(watcher.Path);
         }
-        
-       /// <summary>
-       /// Hundle for Delete
-       /// </summary>
-       /// <param name="sender"></param>
-       /// <param name="e"></param>
+
+        private void ReViewingDirectory(string directoryPath)
+        {
+            DirectoryInfo dir = new DirectoryInfo(directoryPath);
+            FileInfo[] files = dir.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                if(Path.GetExtension(file.FullName) == ".xml" || Path.GetExtension(file.FullName) == ".csv")
+                {
+                    LogManager.DoLogging(LogType.Appearance, null, $"{Path.GetFileName(file.FullName)} was appeared.");
+                    storeData.collection.Add(file.FullName);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Hundle for Delete
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Watcher_Deleted(object sender, FileSystemEventArgs e)
         {
             LogManager.DoLogging(LogType.Delete, null, "File was deleted from monitoring directory.");
         }
 
-       /// <summary>
-       /// Hundle for Create
-       /// </summary>
-       /// <param name="sender"></param>
-       /// <param name="e"></param>
+        /// <summary>
+        /// Hundle for Create
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Watcher_Created(object sender, FileSystemEventArgs e)
         {
             string extension = Path.GetExtension(e.FullPath);
@@ -76,6 +92,6 @@ namespace BLL
                 storeData.collection.Add(e.FullPath);
             }
         }
-            
+
     }
 }
